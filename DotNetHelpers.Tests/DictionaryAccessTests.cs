2b9e5674fe7db5544be312b69deb71056d9ed1be
Cleomar Cantil <cleomarcantil@gmail.com>
2024-11-03 17:41:50 -0300

    Melhorias e ajustes de nomes.

---
+++ DotNetHelpers.Tests/DictionaryAccessTests.cs
@@ -0,0 +1,91 @@
+﻿using DotNetHelpers.Collections;
+
+namespace DotNetHelpers.Tests;
+
+[TestClass]
+public sealed class DictionaryAccessTests
+{
+    [TestMethod]
+    public void Count_sem_dicionario_inicializado_deve_ser_zero()
+    {
+        var values = new DictionaryAccess<string, object>(null);
+
+        Assert.AreEqual(0, values.Count);
+    }
+
+    [TestMethod]
+    public void Count_com_dicionario_sem_itens_deve_ser_zero()
+    {
+        Dictionary<string, object> sourceDic = new();
+        var values = new DictionaryAccess<string, object>(sourceDic);
+
+        Assert.AreEqual(0, values.Count);
+    }
+
+    [TestMethod]
+    public void Pegar_valor_de_nome_sem_dicionario_inicializado_deve_retornar_nulo()
+    {
+        var values = new DictionaryAccess<string, object>(null);
+
+        var x = values["x"];
+
+        Assert.IsNull(x);
+    }
+
+    [TestMethod]
+    public void Pegar_valor_de_nome_inexistente_de_dicionario_vazio_deve_retornar_nulo()
+    {
+        Dictionary<string, object> sourceDic = new();
+        var values = new DictionaryAccess<string, object>(sourceDic);
+
+        var x = values["x"];
+
+        Assert.IsNull(x);
+    }
+
+    [TestMethod]
+    public void Pegar_Count__deve_corresponder_com_quantidade_de_chaves_no_dicionário()
+    {
+        Dictionary<string, object> sourceDic = new()
+        {
+            ["x1"] = 99,
+            ["x2"] = 98
+        };
+
+        var values = new DictionaryAccess<string, object>(sourceDic);
+
+        Assert.AreEqual(2, values.Count);
+    }
+
+    [TestMethod]
+    public void Pegar_valor_de_nome_deve_ser_o_mesmo_no_dicionario()
+    {
+        const int VALOR_TESTE = 99;
+        Dictionary<string, object> sourceDic = new()
+        {
+            ["x"] = VALOR_TESTE,
+        };
+
+        var values = new DictionaryAccess<string, object>(sourceDic);
+        var x = values["x"];
+
+        Assert.AreEqual(VALOR_TESTE, x);
+    }
+
+    [TestMethod]
+    public void Definir_valores_e_enumerar_deve_corresponder()
+    {
+        Dictionary<string, object> sourceDic = new()
+        {
+            ["x1"] = 99,
+            ["x2"] = 98,
+            ["x3"] = 97,
+        };
+        var sourceValues = sourceDic.Select(x => (x.Key, x.Value)).ToList();
+
+        var values = new DictionaryAccess<string, object>(sourceDic);
+        var result = Enumerable.ToArray(values);
+
+        CollectionAssert.AreEquivalent(sourceValues, result);
+    }
+}
---
+++ DotNetHelpers/Collections/DictionaryAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace DotNetHelpers.Collections;
+
+public class DictionaryAccess<TKey, TValue>(IDictionary<TKey, TValue>? sourceDictionary) 
+    : IReadOnlyCollection<(TKey name, TValue value)>
+{
+    public virtual TValue? this[TKey name]
+        => (sourceDictionary is { } dic && dic.TryGetValue(name, out var v)) ? v : default;
+
+    public int Count
+        => sourceDictionary?.Count ?? 0;
+
+    IEnumerator<(TKey name, TValue value)> IEnumerable<(TKey name, TValue value)>.GetEnumerator()
+        => (sourceDictionary?.Select(kv => (kv.Key, kv.Value)) ?? []).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IReadOnlyCollection<(TKey, TValue)>)this).GetEnumerator();
+}
