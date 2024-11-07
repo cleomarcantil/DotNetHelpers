aa663489013f663a8377de5237bd672b60a96453
Cleomar Cantil <cleomarcantil@gmail.com>
2024-11-06 22:54:15 -0300

    Inclusão de NotifyChangesDebounce.

---
+++ DotNetHelpers.Tests/NotifyChangesDebounceTests.cs
@@ -0,0 +1,70 @@
+﻿using DotNetHelpers.Notification;
+
+namespace DotNetHelpers.Tests;
+
+[TestClass]
+public sealed class NotifyChangesDebounceTests
+{
+    [TestMethod]
+    public async Task Monitore_callback_nao_deve_ser_disparado_sem_chamada_de_NotifyChanged()
+    {
+        NotifyChangesDebounce<string, int> notifyChanges = new(50);
+        CancellationTokenSource cts = new(200);
+
+        int changesCount = 0;
+        await notifyChanges.Monitore(async changes =>
+        {
+            changesCount = changes.Count;
+            Assert.Fail("Callback disparado!");
+        }, cts.Token);
+
+        Assert.AreEqual(0, changesCount);
+    }
+
+    [TestMethod]
+    public async Task Changes_em_Monitore_callback_deve_corresponder_as_chamadas_de_NotifyChanged_com_keys_diferentes()
+    {
+        NotifyChangesDebounce<string, int> notifyChanges = new(50);
+        CancellationTokenSource cts = new(60_000); // Ao debugar, levar em conta o timeout
+        (string name, int value)[] sourceValues =
+            [
+                ("a", 99),
+                ("b", 88),
+                ("c", 77)
+            ];
+
+        foreach (var (n, v) in sourceValues)
+        {
+            notifyChanges.NotifyChanged(n, v);
+        }
+
+        (string name, int value)[]? changesResult = null;
+        await notifyChanges.Monitore(async changes =>
+        {
+            changesResult = changes.ToArray();
+            cts.Cancel();
+        }, cts.Token);
+
+        CollectionAssert.AreEquivalent(sourceValues, changesResult);
+    }
+
+    [TestMethod]
+    public async Task Chamadas_sucessivas_de_NotifyChanged_com_mesma_key_deve_gerar_apenas_uma_change_com_ultimo_valor_em_Monitore_callback()
+    {
+        NotifyChangesDebounce<string, int> notifyChanges = new(50);
+        CancellationTokenSource cts = new(60_000); // Ao debugar, levar em conta o timeout
+        
+        notifyChanges.NotifyChanged("a", 99);
+        notifyChanges.NotifyChanged("a", 88);
+        notifyChanges.NotifyChanged("a", 77);
+
+        (string name, int value)[]? changesResult = null;
+        await notifyChanges.Monitore(async changes =>
+        {
+            changesResult = changes.ToArray();
+            cts.Cancel();
+        }, cts.Token);
+
+        CollectionAssert.AreEquivalent(new[] { ("a", 77) }, changesResult);
+    }
+}
---
+++ DotNetHelpers/Notification/NotifyChangesDebounce.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+namespace DotNetHelpers.Notification;
+
+public class NotifyChangesDebounce<TKey, TValue>(int interval) : IDisposable
+{
+    private readonly ConcurrentDictionary<TKey, TValue> _changes = new();
+    private readonly ConcurrentDictionary<string, ChangedCallback> _monitors = new();
+
+    private object _changedLock = new();
+    private DateTime? _lastChangedTime;
+
+    private readonly object _monitoringTaskLock = new();
+    private Task? _monitoringTask;
+    private CancellationTokenSource _monitoringTaskToken = new();
+
+    public void Dispose()
+    {
+        _monitoringTaskToken.Cancel();
+        _monitoringTaskToken.Dispose();
+    }
+
+    public void NotifyChanged(TKey key, TValue value)
+    {
+        lock (_changedLock)
+        {
+            _changes.AddOrUpdate(key, (_) => value, (_, _) => value);
+            _lastChangedTime = DateTime.Now;
+        }
+    }
+
+    private InternalChangesAccess? ExtractChangesToNotify()
+    {
+        lock (_changedLock)
+        {
+            if (_lastChangedTime is null)
+                return null;
+            
+            var changesToNotify = _changes.ToArray();
+            _changes.Clear();
+            _lastChangedTime = null;
+
+            return new InternalChangesAccess(changesToNotify);
+        }
+    }
+
+    public async Task Monitore(ChangedCallback changedCallback, CancellationToken cancellationToken)
+    {
+        var monitorKey = Guid.NewGuid().ToString();
+
+        if (_monitors.TryAdd(monitorKey, changedCallback))
+        {
+            EnsureStartedMonitoringTask();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Delay(interval, cancellationToken);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            _monitors.TryRemove(monitorKey, out var _);
+        }
+    }
+
+    private void EnsureStartedMonitoringTask()
+    {
+        lock (_monitoringTaskLock)
+        {
+            if (_monitoringTask is null)
+            {
+                _monitoringTask = RunMonitoring(
+                    onComplete: () =>
+                    {
+                        lock (_monitoringTaskLock)
+                        {
+                            _monitoringTask = null;
+                        }
+                    });
+            }
+        }
+    }
+
+    private async Task RunMonitoring(Action onComplete)
+    {
+        while (!_monitors.IsEmpty && !_monitoringTaskToken.IsCancellationRequested)
+        {
+            await Task.Delay(interval, _monitoringTaskToken.Token);
+
+            if (ExtractChangesToNotify() is { Count: > 0 } changesToNotify)
+            {
+                foreach (var (_, callback) in _monitors)
+                {
+                    try
+                    {
+                        await callback.Invoke(changesToNotify);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+
+        onComplete.Invoke();
+    }
+
+    public delegate Task ChangedCallback(IReadOnlyCollection<(TKey key, TValue value)> changes);
+
+    private class InternalChangesAccess(KeyValuePair<TKey, TValue>[]? changes) 
+        : IReadOnlyCollection<(TKey key, TValue value)>
+    {
+        public int Count 
+            => (changes?.Length ?? 0);
+
+        public IEnumerator<(TKey key, TValue value)> GetEnumerator()
+            => (changes?.Select(c => (c.Key, c.Value)) ?? []).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
