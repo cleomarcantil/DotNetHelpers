6c886562dd9e75a9f4039b60f04196048ead95f4
Cleomar Cantil <cleomarcantil@gmail.com>
2024-11-09 15:33:06 -0300

    Inclusão de OrderedQueue.

---
+++ DotNetHelpers.Tests/OrderedQueueTests.cs
@@ -0,0 +1,180 @@
+﻿using DotNetHelpers.Collections;
+using System.Collections.Concurrent;
+
+namespace DotNetHelpers.Tests;
+
+[TestClass]
+public sealed class OrderedQueueTests
+{
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void Valores_aleatorios_devem_ser_enumerados_em_ordem(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        var orderedValues = randomValues.Order().ToArray();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        var valuesInQueue = oq.ToArray();
+
+        CollectionAssert.AreEqual(orderedValues, valuesInQueue);
+    }
+
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void Dequeue_retora_o_primeiro_da_ordem(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        var minValue = randomValues.Min();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        int? r1 = oq.TryDequeue(out var x) ? x : null;
+
+        Assert.IsNotNull(r1);
+        Assert.AreEqual(minValue, r1);
+    }
+
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void Dequeue_deve_remover_da_fila(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        int totalCount = randomValues.Length;
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        int count_initial = oq.Count;
+        oq.TryDequeue(out var _);
+        int count_less_1 = oq.Count;
+        oq.TryDequeue(out var _);
+        int count_less_2 = oq.Count;
+        oq.TryDequeue(out var _);
+        int count_less_3 = oq.Count;
+
+        Assert.AreEqual(totalCount, count_initial);
+        Assert.AreEqual(totalCount - 1, count_less_1);
+        Assert.AreEqual(totalCount - 2, count_less_2);
+        Assert.AreEqual(totalCount - 3, count_less_3);
+    }
+
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void Apos_Dequeue_proximos_valores_devem_permanecer(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        var orderedValuesExceptFirst = randomValues.Order().Skip(1).ToArray();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        oq.TryDequeue(out var _);
+        var valuesInQueue = oq.ToArray();
+
+        CollectionAssert.AreEqual(orderedValuesExceptFirst, valuesInQueue);
+    }
+
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void DequeueIf_deve_remover_o_primeiro_item_da_fila_que_atende_a_condicao(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        var minValue = randomValues.Min();
+        var orderedValuesExceptFirst = randomValues.Order().Skip(1).ToArray();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        int? r1 = oq.TryDequeueIf(v => v == minValue, out var x) ? x : null;
+        var valuesInQueue = oq.ToArray();
+
+        Assert.IsNotNull(r1);
+        Assert.AreEqual(minValue, r1);
+        CollectionAssert.AreEqual(orderedValuesExceptFirst, valuesInQueue);
+    }
+
+    [TestMethod]
+    [DataRow(false, DisplayName = "concurrencyLock: false")]
+    [DataRow(true, DisplayName = "concurrencyLock: true")]
+    public void DequeueIf_nao_deve_remover_o_primeiro_item_da_fila_que_nao_atende_a_condicao(bool concurrencyLock)
+    {
+        int[] randomValues = [5, 2, 4, 1, 3];
+        var maxValue = randomValues.Max();
+        var orderedValues = randomValues.Order().ToArray();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock);
+        int? r1 = oq.TryDequeueIf(v => v == maxValue, out var x) ? x : null;
+        var valuesInQueue = oq.ToArray();
+
+        Assert.IsNull(r1);
+        CollectionAssert.AreEqual(orderedValues, valuesInQueue);
+    }
+
+    #region Concurrent tests
+
+    [TestMethod]
+    public void Adicionar_valores_paralelamente_deve_manter_a_integridade()
+    {
+        int[] randomValues = Enumerable.Range(1, 1000).Select(n => Random.Shared.Next(100)).ToArray();
+        var orderedValues = randomValues.Order().ToArray();
+
+        OrderedQueue<int> oq = new(concurrencyLock: true);
+        Parallel.ForEach(randomValues, v => oq.Add(v));
+        var valuesInQueue = oq.ToArray();
+
+        CollectionAssert.AreEqual(orderedValues, valuesInQueue);
+    }
+
+    [TestMethod]
+    [DataRow(10, DisplayName = "10 números aleatórios")]
+    [DataRow(100, DisplayName = "100 números aleatórios")]
+    [DataRow(1000, DisplayName = "1000 números aleatórios")]
+    [DataRow(10000, DisplayName = "10000 números aleatórios")]
+    public void Dequeue_paralelamente_deve_retornar_valores_correspondentes_aos_primeiros_da_ordem(int randomSize)
+    {
+        int dequeueCount = randomSize / 3;
+        int[] randomValues = GenerateRandomValues(1, randomSize);
+        var orderedValues = randomValues.Order().ToArray();
+        var firstOrderedValues = orderedValues.Take(dequeueCount).ToArray();
+
+        ConcurrentBag<int?> dequeuedValues = new();
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock: true);
+        Parallel.For(0, dequeueCount, _ => dequeuedValues.Add(oq.TryDequeue(out var v) ? v : null));
+        var orderedDequeuedValues = dequeuedValues.ToArray().Order().ToArray();
+
+        CollectionAssert.AreEqual(firstOrderedValues, orderedDequeuedValues);
+    }
+
+
+    [TestMethod]
+    [DataRow(10, DisplayName = "10 números aleatórios")]
+    [DataRow(100, DisplayName = "100 números aleatórios")]
+    [DataRow(1000, DisplayName = "1000 números aleatórios")]
+    [DataRow(10000, DisplayName = "10000 números aleatórios")]
+    public void Dequeue_paralelamente_deve_manter_valores_restantes_integros(int randomSize)
+    {
+        int dequeueCount = randomSize / 3;
+        int[] randomValues = GenerateRandomValues(1, randomSize);
+        var orderedValues = randomValues.Order().ToArray();
+        var remainderOrderedValues = orderedValues.Skip(dequeueCount).ToArray();
+
+        OrderedQueue<int> oq = new(randomValues, concurrencyLock: true);
+        Parallel.For(0, dequeueCount, _ => oq.TryDequeue(out var _));
+        var valuesInQueue = oq.ToArray();
+
+        CollectionAssert.AreEqual(remainderOrderedValues, valuesInQueue);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private int[] GenerateRandomValues(int start, int end)
+    {
+        var numbers = Enumerable.Range(start, end).ToArray();
+
+        Random.Shared.Shuffle(numbers);
+
+        return numbers;
+    }
+
+    #endregion
+}
---
+++ DotNetHelpers/Collections/OrderedQueue.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+
+namespace DotNetHelpers.Collections;
+
+public class OrderedQueue<T> : IReadOnlyCollection<T>
+    where T : IComparable<T>
+{
+    private readonly SortedList<KeyItem, object?> _values = new();
+    private long _sequence = 0;
+    private readonly object? _lockValues = null;
+
+
+    public OrderedQueue(IEnumerable<T> values, bool concurrencyLock = false)
+    {
+        if (concurrencyLock)
+            _lockValues = new();
+
+        AddRange(values);
+    }
+
+    public OrderedQueue(bool concurrencyLock = false)
+        : this([], concurrencyLock)
+    {
+    }
+
+    public void Add(T value)
+        => AddRange(value);
+
+    public void AddRange(params IEnumerable<T> values)
+        => OnLockContext(() =>
+        {
+            foreach (var v in values)
+            {
+                _values.Add(new(v, ++_sequence), null);
+            }
+        });
+
+    public bool TryDequeue(out T result)
+        => TryDequeueIf(x => true, out result);
+
+    public bool TryDequeueIf(Func<T, bool> predicade, out T value)
+    {
+        (var dequeued, value) = OnLockContext(() =>
+        {
+            var key = (_values.Count > 0) ? _values.GetKeyAtIndex(0) : null;
+
+            if (key is not null && predicade(key.Value) && _values.Remove(key))
+                return (true, key.Value);
+            else
+                return (false, default!);
+        });
+
+        return dequeued;
+    }
+
+
+    #region IReadOnlyCollection
+
+    public int Count
+        => _values.Count;
+
+    public IEnumerator<T> GetEnumerator()
+        => _values.Keys.Select(x => x.Value).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+
+    #endregion
+
+    #region Internal
+
+    private R OnLockContext<R>(Func<R> action)
+    {
+        if (_lockValues != null)
+            lock (_lockValues) return action.Invoke();
+        else
+            return action.Invoke();
+    }
+
+    private void OnLockContext(Action action)
+        => OnLockContext<object?>(() => { action.Invoke(); return null; });
+
+    record KeyItem(T Value, long Sequence) : IComparable<KeyItem>
+    {
+        public int CompareTo(KeyItem? other)
+            => (Value.CompareTo(other!.Value)) switch
+            {
+                0 => Sequence.CompareTo(other.Sequence),
+                var c => c,
+            };
+    }
+
+    #endregion
+}
